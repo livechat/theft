142527f86c61dc0f7fe1f58fff9f0694c0917db6
morus12 <g.wyszynski@livechatinc.com>
2015-07-28 13:42:19 +0200

    pass port, domain, secure as argv

--- settings.go
+++ settings.go
@@ -7,6 +7,9 @@ import (
 type Settings struct {
 	logLevel *int
 	logPath *string
+	port *string
+	domain *string
+	secure *bool
 }
 
 var settings *Settings
@@ -15,5 +18,8 @@ func parseFlags() {
   settings = &Settings{}
   settings.logLevel = flag.Int("log-level", ERROR, "log level 0 - 15")
   settings.logPath = flag.String("log-path", "", "path to file where logs are stored")
+  settings.port = flag.String("port", ":8080", "server port")
+  settings.domain = flag.String("domain", "localhost", "server domain")
+  settings.secure = flag.Bool("secure", false, "use secure urls (wss, https protocols)")
   flag.Parse()
 }
